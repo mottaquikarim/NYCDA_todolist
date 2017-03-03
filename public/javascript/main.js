cd86372004a6d6d2c8b61317c45296a24a8c29da
Mottaqui Karim <taqqui.karim@gmail.com>
2017-03-02 22:13:00 -0500

    implemented get and post methods on FE

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,1 +1,52 @@
-/* CURRENTLY IN: javascript/main.js */
+(function() { // protect the lemmings
+
+	function GET(url) {
+		return new Promise((resolve, reject) => {
+			const request = new XMLHttpRequest();
+			request.open('GET', url);
+			request.onload = () => {
+				const data = JSON.parse(request.responseText);
+				resolve(data)
+			}; 
+			request.onerror = (err) => {
+				reject(err)
+			};
+			request.send();
+		});
+	} // GET
+
+	function POST(url, data) {
+		return new Promise((resolve, reject) => {
+			const request = new XMLHttpRequest();
+			request.open('POST', url);
+			request.setRequestHeader('Content-Type', 'application/json');
+
+			request.onload = () => {
+				const data = JSON.parse(request.responseText);
+				resolve(data)
+			}; 
+			request.onerror = (err) => {
+				reject(err)
+			};
+
+			request.send(JSON.stringify(data));
+		});
+	} // POST
+
+
+	GET('/api/todos')
+		.then((data) => {
+			console.log(data);
+
+			return POST('/api/todos', {
+				todo: 'wake up',
+				when: new Date().getTime() + 9 * 60 * 60 * 1000
+			});
+
+		})
+		.then((dataFromPostLOL) => {
+			console.log(dataFromPostLOL);
+		});
+
+})();
+
